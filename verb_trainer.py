938e3eeabf124d6d59bbff8e27e2f014acc5a823
Kevin Krawiec <krawieckevin03@gmail.com>
2026-09-06 18:16:13 +0200

    final program

--- verb_trainer.py
+++ verb_trainer.py
@@ -1,18 +1,36 @@
 import json
 import random
 
-with open("irregular-verbs.json", "r", encoding="utf-8") as plik:
-    czasowniki = json.load(plik)
+def read_file():
+    with open("irregular-verbs.json", "r", encoding="utf-8") as f:
+        verbs = json.load(f)
+        return verbs
 
-print(czasowniki[0])
-print(czasowniki[0]["past"])
+def check(answer, correct_answer):
+    return answer in correct_answer
 
-while True:
-    user_answer = input(f"Podaj 2 forme {czasowniki[0]["pl"]}: ")
-    if user_answer in czasowniki[0]["past"]:
-        print("dobrze")
-        break
-    else:
-        for v in czasowniki[0].values():
-            print(v, end=", ")
-        print("\nspróbuj ponownie")
+def ask(verbs):
+    verb = random.choice(verbs)
+
+    while True:
+        user_answer = input(f"Podaj trzy nieregularne formy czasownika {verb["pl"]}: ").lower()
+        if user_answer == "q" or user_answer == "koniec":
+            return False
+
+        parts = user_answer.split()
+
+        if len(parts) != 3:
+            print("Podaj 3 formy oddzielone spacją!")
+            continue
+
+        a, b, c = parts
+
+        if check(a, verb["base"]) and check(b, verb["past"]) and check(c, verb["participle"]):
+            return True
+
+        print(f"Źle. Poprawnie: {' / '.join(verb["base"])}, {' / '.join(verb["past"])}, {' / '.join(verb["participle"])}")
+
+if __name__ == "__main__":
+    verbs = read_file()
+    while ask(verbs):
+        pass
